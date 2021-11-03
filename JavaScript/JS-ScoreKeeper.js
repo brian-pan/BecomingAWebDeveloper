e233655e5784ea0cfc820dacbbdce921f170dc47
Brian Pan <brianpanzifan@gmail.com>
2021-11-03 17:49:17 -0400

    scoreKeeper 2nd

--- JavaScript/JS-ScoreKeeper.js
+++ JavaScript/JS-ScoreKeeper.js
@@ -4,12 +4,17 @@ const resetButton = document.querySelector('#resetButton');
 
 const p1DisPlay = document.querySelector('#p1Display'); //span
 const p2DisPlay = document.querySelector('#p2Display');
+const roundsSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
 let isGameOver = false;
 
+roundsSelect.addEventListener('change', function () {
+    winningScore = parseInt(roundsSelect.value);
+    reset();
+})
 p1Button.addEventListener('click', function () {
     if (!isGameOver) {
         p1Score ++;
@@ -28,3 +33,12 @@ p2Button.addEventListener('click', function () {
         p2DisPlay.textContent = p2Score;
     }
 })
+resetButton.addEventListener('click', reset)
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1DisPlay.textContent = p1Score;
+    p2DisPlay.textContent = p2Score;
+}
