918f4f7f50145058cf5ad35d638ab9468ed64f19
Brian Pan <brianpanzifan@gmail.com>
2021-12-08 19:19:04 -0500

    initial commit

---
+++ JavaScript/JS-Express/FirstApp/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+
+// app.use((req, res) => {
+//     console.log('We got a new request.');
+//     res.send('<h1>This is my Webpage!</h1>')
+// })
+
+// root route:
+app.get('/', (req, res) => {
+    res.send('This is the home page!')
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit...`)
+})
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+    const {subreddit, postId} = req.params;
+    res.send(`<h1>Viewing Post ID: ${postId} on the ${subreddit} subreddit...`)
+})
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW!!!')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!')
+})
+
+app.post('/cats', (req, res) => {
+    res.send('Post request to /cats')
+})
+
+app.get('*', (req, res) => {
+    res.send(`I don't know that path.`)
+})
+
+app.listen(8080, () => {
+    console.log('Listen on port 8080:');
+})
