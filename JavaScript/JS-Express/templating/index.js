c54d205c482fccac70949ed7cb1711093a67fa0f
Brian Pan <brianpanzifan@gmail.com>
2021-12-09 23:59:35 -0500

    .

--- JavaScript/JS-Express/templating/index.js
+++ JavaScript/JS-Express/templating/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
+// before we use:
+// app.get('/', (req, res) => {
+//     res.send('HI');
+// })
+// Now change to:
 app.get('/', (req, res) => {
-    res.send('HI');
+    res.render('home.ejs')
 })
 
 app.listen(3000, () => {
