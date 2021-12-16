9d87313c9444b30aa149cd93b4371def0702efac
Brian Pan <brianpanzifan@gmail.com>
2021-12-16 04:33:25 -0500

    update

--- JavaScript/JS-Express/RESTDemo/index.js
+++ JavaScript/JS-Express/RESTDemo/index.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 //parse the request body as URL-encoded data
 // (tell express how to parse it, o/w it will be undefined)
 app.use(express.urlencoded({ extended: true }))
 //parsing json data into request body
 app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+//fake some data, since we don't have database yet:
+let comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+//get comments:
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
 
 app.get('/tacos', (req, res) => {
     res.send('Get response of /tacos')
