6103621f72534a4725f21c18293e38ef008b18b5
Brian Pan <brianpanzifan@gmail.com>
2022-03-14 04:34:00 -0400

    farm model

--- JavaScript/MongoExpressRelation/index.js
+++ JavaScript/MongoExpressRelation/index.js
@@ -18,11 +18,63 @@ mongoose.connect('mongodb://localhost:27017/farmStandTake2', { useNewUrlParser: 
         console.log(err)
     })
 
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
+
+// FARM ROUTES
+
+app.get('/farms', async (req, res) => {
+    const farms = await Farm.find({});
+    res.render('farms/index', { farms })
+})
+app.get('/farms/new', (req, res) => {
+    res.render('farms/new')
+})
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id).populate('products');
+    res.render('farms/show', { farm })
+})
+
+app.delete('/farms/:id', async (req, res) => {
+    const farm = await Farm.findByIdAndDelete(req.params.id);
+
+    res.redirect('/farms');
+})
+
+
+
+
+app.post('/farms', async (req, res) => {
+    const farm = new Farm(req.body);
+    await farm.save();
+    res.redirect('/farms')
+})
+
+app.get('/farms/:id/products/new', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories, farm })
+})
+
+app.post('/farms/:id/products', async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`/farms/${id}`)
+})
+
+
+
+// PRODUCT ROUTES
 
 app.get('/products', async (req, res) => {
     const { category } = req.query;
@@ -69,6 +121,11 @@ app.delete('/products/:id', async (req, res) => {
     res.redirect('/products');
 })
 
+
+
 app.listen(3000, () => {
     console.log("APP IS LISTENING ON PORT 3000!")
 })
+
+
+
